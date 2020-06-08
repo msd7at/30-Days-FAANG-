0efe9b6426699ff1302fe57da3b07307295855be
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-06-09 00:59:12 +0530

    Create Find duplicates in an array.py

---
+++ Find duplicates in an array.py
@@ -0,0 +1,19 @@
+Question link:
+GFG:-https://practice.geeksforgeeks.org/problems/find-duplicates-in-an-array/1
+O(n)space and Time Solution
+
+def duplicates(arr, n): 
+	d=[0]*n
+	l=[]
+	p=0
+	for i in arr:
+	    d[i]+=1
+	    if d[i]==2:
+	        p=8
+	if p==0:
+	    return [-1]
+	else:
+	    for i in range(n):
+	        if d[i]>1:
+	            l.append(i)
+	    return l
