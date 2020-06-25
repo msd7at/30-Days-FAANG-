5e084647a5408c496f4d55a781940386618fec60
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-06-25 05:33:46 +0530

    Create 1.6 merge Overlapping Intervals.py

---
+++ 1.6 merge Overlapping Intervals.py
@@ -0,0 +1,30 @@
+#link : = https://practice.geeksforgeeks.org/problems/overlapping-intervals/0
+t=int(input())
+for q in range(t):
+    n=int(input())
+    ar=[]
+    ti=[int(b) for b in input().split()]
+    for i in range(n):
+        jk=[]
+        jk.append(ti[2*i])
+        jk.append(ti[2*i+1])
+        ar.append(jk)
+    ar.sort()
+    m=[]
+    s=-10000
+    max= -10000
+    for i in range(n):
+        a=ar[i]
+        if a[0]>max:
+            if i!=0 :
+                m.append([s,max])
+            max=a[1]
+            s=a[0]
+        else:
+            if a[1]>=max:
+                max=a[1]
+    if max!= -10000 and [s,max] not in m:
+        m.append([s,max])
+    for i in m:
+        print(i[0],i[1],end=" ")
+    print()
