00036016864837bae896300f3e07c93939b9732f
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-06-30 05:45:42 +0530

    Create 5.1 1. Reverse a LinkedList .py

---
+++ 5.1 1. Reverse a LinkedList .py
@@ -0,0 +1,18 @@
+#link  https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur=head
+        temp=None
+        while cur:
+            nxt=cur.next
+            cur.next=temp
+            temp=cur
+            cur=nxt
+        return temp
