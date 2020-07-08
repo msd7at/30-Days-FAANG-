5c65844bd56e1d05bb1f286bc0fcf748048cbbf7
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-07-08 05:47:18 +0530

    Create T 13.1 Stack using Queue.py

---
+++ T 13.1 Stack using Queue.py
@@ -0,0 +1,42 @@
+# link :  https://leetcode.com/problems/implement-stack-using-queues/
+
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue=deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        temp=deque([x])
+        temp.extend(self.queue)
+        self.queue=temp
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.queue)==0
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
