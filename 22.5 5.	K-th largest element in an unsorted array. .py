08107a7a1486dd5cbfdcbbece24d3c68728376ae
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-07-28 04:26:00 +0530

    Create 22.5 5.	K-th largest element in an unsorted array. .py

---
+++ 22.5 5.	K-th largest element in an unsorted array. .py
@@ -0,0 +1,9 @@
+#link: https://leetcode.com/problems/kth-largest-element-in-an-array/
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+        for _ in range(len(nums)-k):
+            heapq.heappop(heap)
+        return heapq.heappop(heap)
