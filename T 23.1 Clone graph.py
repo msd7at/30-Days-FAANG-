cfaeee891ec47fb96ef46b822ecc89e96276184a
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-07-29 05:02:57 +0530

    Create T 23.1 Clone graph.py

---
+++ T 23.1 Clone graph.py
@@ -0,0 +1,27 @@
+# link: https://leetcode.com/problems/clone-graph/
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return 
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy}
+        queue = collections.deque([node])
+        while queue:
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor not in dic: # neighbor is not visited
+                    neighborCopy = Node(neighbor.val, [])
+                    dic[neighbor] = neighborCopy
+                    dic[node].neighbors.append(neighborCopy)
+                    queue.append(neighbor)
+                else:
+                    dic[node].neighbors.append(dic[neighbor])
+        return nodeCopy
