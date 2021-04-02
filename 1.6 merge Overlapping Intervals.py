fe487e5e31c7d76fe98c35b796c719135063c9d1
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2021-04-03 00:28:07 +0530

    Update 1.6 merge Overlapping Intervals.py

--- 1.6 merge Overlapping Intervals.py
+++ 1.6 merge Overlapping Intervals.py
@@ -1,3 +1,17 @@
+
+#leetcode
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans=[]
+        for i in intervals:
+            if not ans or ans[-1][1] < i[0]:
+                ans.append(i)
+            else:
+                ans[-1][1]=max(ans[-1][1],i[1])
+        return ans
+    
+    
 #link : = https://practice.geeksforgeeks.org/problems/overlapping-intervals/0
 t=int(input())
 for q in range(t):
